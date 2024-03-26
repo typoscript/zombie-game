49360918879b3db931fd5f460a5671225d8ad51c
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 14:35:10 +0900

    Add attack() Method

--- zombie-game/src/zombie/Zombie.java
+++ zombie-game/src/zombie/Zombie.java
@@ -6,4 +6,20 @@ public class Zombie extends Unit {
 	public Zombie(int position, int hp, int maxHp) {
 		super(position, hp, maxHp);
 	}
+	
+	public void attack(Unit hero) {
+		final int MAX_ATTACK_POWER = 10;
+		final int MIN_ATTACK_POWER = 1;
+
+		attackPower = (int)(Math.random() * MAX_ATTACK_POWER) + MIN_ATTACK_POWER;
+		
+		hero.damageHp(attackPower);
+		
+		this.healHp(attackPower / 2);
+		
+		System.out.printf(
+			"좀비가 공격력 %d로 공격 | 공격 후 Hero의 HP: %d | 공격으로 체력 회복 후 좀비의 체력: %d\n",
+			attackPower, hero.getHp(), getHp()
+		);
+	}
 }

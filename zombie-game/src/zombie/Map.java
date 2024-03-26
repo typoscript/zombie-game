350da9eb0cfc2e6f6ee6195d2ce1e4cd2b9cf4ab
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 16:39:02 +0900

    Add Member Variables

--- zombie-game/src/zombie/Map.java
+++ zombie-game/src/zombie/Map.java
@@ -1,4 +1,11 @@
 package zombie;
 
 public class Map {
+	private final int MAP_SIZE = 10;
+
+	public static final int HERO_POSITION = 0;
+	public static final int ZOMBIE_POSITION = 5;
+	public static final int BOSS_POSITION = 9;
+	
+	private Unit[] map = new Unit[MAP_SIZE];
 }

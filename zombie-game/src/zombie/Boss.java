f25d205148fc706e5908dbcd3c1201f31f7f3121
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 14:38:02 +0900

    Add Constructor Method

--- zombie-game/src/zombie/Boss.java
+++ zombie-game/src/zombie/Boss.java
@@ -2,4 +2,9 @@ package zombie;
 
 public class Boss extends Zombie {
 	private int shield;
+	
+	public Boss(int position, int hp, int maxHp, int shield) {
+		super(position, hp, maxHp);
+		this.shield = shield;
+	}
 }

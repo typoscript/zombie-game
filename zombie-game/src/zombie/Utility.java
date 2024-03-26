c444398e59258d673fcd1779044be93b01366247
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 18:35:23 +0900

    Add Scanner Member Variable

--- zombie-game/src/zombie/Utility.java
+++ zombie-game/src/zombie/Utility.java
@@ -1,6 +1,10 @@
 package zombie;
 
+import java.util.Scanner;
+
 public class Utility {
+	public static Scanner scanner = new Scanner(System.in);
+
 	public static int getRandomNumber(int min, int max) {
 		return (int)(Math.random() * ((max + 1) - min)) + min;
 	}

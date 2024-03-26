41c9591cdf58102354e8fd9ea51f7c6025848dc9
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 15:39:01 +0900

    Refactor getRandomNumber() Method

--- zombie-game/src/zombie/Boss.java
+++ zombie-game/src/zombie/Boss.java
@@ -20,9 +20,7 @@ public class Boss extends Zombie {
 		final int MIN_ATTACK_POWER = 5;
 		final int MAX_ATTACK_POWER = 20;
 
-		int attackPower = (int)(Math.random() * (MAX_ATTACK_POWER - MIN_ATTACK_POWER + 1)) + MIN_ATTACK_POWER;
-
-		return attackPower;
+		return Utility.getRandomNumber(MIN_ATTACK_POWER, MAX_ATTACK_POWER);
 	}
 	
 	public boolean canDoubleAttackPower() {

fb7b041f505936043c01adcc4a8e9e2f239395a6
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 15:13:02 +0900

    Add getRandomAttackPower() Method

--- zombie-game/src/zombie/Boss.java
+++ zombie-game/src/zombie/Boss.java
@@ -15,4 +15,13 @@ public class Boss extends Zombie {
 	public int getShield() {
 		return this.shield;
 	}
+	
+	public int getRandomAttackPower() {
+		final int MIN_ATTACK_POWER = 5;
+		final int MAX_ATTACK_POWER = 20;
+
+		int attackPower = (int)(Math.random() * (MAX_ATTACK_POWER - MIN_ATTACK_POWER + 1)) + MIN_ATTACK_POWER;
+
+		return attackPower;
+	}
 }

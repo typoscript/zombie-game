2d74edc70db38f937e7ae2765c8b6caf12b18a55
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 15:37:49 +0900

    Fix getRandomNumber() Returning Incorrect Value

--- zombie-game/src/zombie/Utility.java
+++ zombie-game/src/zombie/Utility.java
@@ -2,6 +2,6 @@ package zombie;
 
 public class Utility {
 	public static int getRandomNumber(int min, int max) {
-		return (int)(Math.random() * (max - min + 1)) + min;
+		return (int)(Math.random() * ((max + 1) - min)) + min;
 	}
 }

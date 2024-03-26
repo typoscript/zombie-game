733d031ae147a70e1d180fd252ea71d4908645ab
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 15:27:23 +0900

    Add healByPotion() Method

--- zombie-game/src/zombie/Hero.java
+++ zombie-game/src/zombie/Hero.java
@@ -5,8 +5,22 @@ public class Hero extends Unit {
 	private int numOfPotions;
 	
 	public Hero(int position, int hp, int maxHp, int numOfPotions) {
-		super(position, hp, maxHp)
+		super(position, hp, maxHp);
 		this.numOfPotions = numOfPotions; 
+	}
+	
+	public void healByPotion() {
+		final int POTION_HP_HEAL_AMOUNT = 20;
+
+		if (numOfPotions == 0) {
+			System.out.println("물약이 없습니다");
+			return; 
+		}
+		
+		healHp(POTION_HP_HEAL_AMOUNT);
+		numOfPotions--;
+
+		System.out.printf("물약 사용 후 HP: %d\n", getHp());
 	}
 
 	public void attack(Unit target) {

5654459592cbee29b0d222e5f1a6914da75f196e
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 14:13:27 +0900

    Add Constructor Method

--- zombie-game/src/zombie/Hero.java
+++ zombie-game/src/zombie/Hero.java
@@ -3,4 +3,9 @@ package zombie;
 public class Hero extends Unit {
 	private int attackPower;
 	private int numOfPotions;
+	
+	public Hero(int position, int hp, int maxHp, int numOfPotions) {
+		super(position, hp, maxHp)
+		this.numOfPotions = numOfPotions; 
+	}
 }

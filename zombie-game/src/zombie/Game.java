f2410e56c0bdf04576991b0f63ea1ce2d97761f1
typoscript <minkyu.kim.contact@gmail.com>
2024-03-26 16:13:47 +0900

    Add sc Member Variable and Import java.util.Scanner

--- zombie-game/src/zombie/Game.java
+++ zombie-game/src/zombie/Game.java
@@ -1,7 +1,10 @@
 package zombie;
 
+import java.util.Scanner;
+
 public class Game {
 	private static Game instance = new Game();
+	private Scanner sc = new Scanner(System.in);
 	private Hero hero;
 	private Zombie zombie;
 	private Boss boss;
@@ -13,11 +16,10 @@ public class Game {
 	}
 	
 	public void run() {
-		
 	}
 	
 	private void initCharacters() {
-		this.hero = new Hero(0, 100, 100, 5);
+		this.hero = new Hero(1, 100, 100, 5);
 		this.zombie = new Zombie(5, 100, 100);
 		this.boss = new Boss(9, 200, 200, 5);
 	}
